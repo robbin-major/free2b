93ae7d5dcc3b0d8cbf54757ffc9298dc2f98343c
Robbin Major <robbinmajor@Robbins-MacBook-Pro.local>
2026-06-29 19:42:48 -0500

    ci: increase android build heap

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_template/utils/app_preferences.dart';
 import 'package:flutter_template/utils/auth_session_service.dart';
 import 'package:flutter_template/utils/assets.dart';
